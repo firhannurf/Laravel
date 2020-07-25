de7773e8bd6e75a458cc95b5b742e49d49e2d002
firhannurf <63441025+firhannurf@users.noreply.github.com>
2020-07-25 11:58:35 +0700

    Add files via upload

---
+++ views/blog.blade.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Profile</title>
+  </head>
+  <style>
+    .h1{
+      background-color: FloralWhite; 
+      height:2500; 
+      width:auto; 
+      border-radius: 25px;
+      border-style: solid; 
+      border-color: black; 
+      border-width: 3px;
+    }
+    .port{
+      border-width: 3px;
+      border-radius: 15px;
+      padding: 8px;
+      border-style: solid;
+      border-color: black;
+      background-color: #00CED1;
+    }
+  </style>
+  <body>
+
+<div style="background-color: aquamarine; width: auto; height:1500; border-radius:25px;">
+  <div class="alert alert-success" role="alert" style="text-align: center;" >
+    <h2>Welcome To My Profile</h2>
+  </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#"><h4>Profile</h4><span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#"><h4>Experience</h4></a>
+      <a class="nav-item nav-link" href="{{ url('/porfolio') }}"><h4>Portofolio</h4></a>
+      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><h1></h1></a>
+    </div>
+  </div>
+</nav>
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+    <br>
+    <div class="h1">
+    <br>
+      <div class="text-center">
+        <img src="poto.jpg" class="rounded-circle" width="400px" height="230px" > 
+      </div>
+        <center><h1>Firhan Nur F</h1></center>
+        <center><h6>Pelajar|Editor|Programer|Youtuber</h6></center>
+        <br>
+    </div>
+    
+     <center><div style="background-color: aquamarine; width:1000px; height:300px;">
+     <h3>Experience</h3>
+        <img src="kunjin1.jpg" class="rounded float-left" width="400px" height="250px; border-radius: 15px;">
+        <img src="kunjin2.jpg" class="rounded float-right" width="350px" height="230px; border-radius: 15px;">
+      </div></center>
+
+      <center><div style="background-color: aquamarine; width:1000px; height:150px;">
+      <h4  class="rounded float-left" >Foto saat saya sedang kunjungan industri di suatu perusahaan yang mengembangkan sebuah Website saat kelas 10</h4>
+      </div></center> 
+<hr>
+      <center><div style="background-color: aquamarine; width:1000px; height:230px;">
+        <img src="bang.jpeg" class="rounded" width="350px" height="230px; border-radius: 15px;">
+      </div></center>
+      <center><div style="background-color: aquamarine; width:1000px; height:150px;">
+      <h4  class="rounded float-left" >Foto saat saya mengikuti workshop di Tebet Jakarta selatan bersama PermataBank dan PermataHati</h4>
+      </div></center> 
+<hr>
+
+      <div class="port">
+          <center><div style="background-color: aquamarine; width:1000px; height:380px; border-radius: 15px;">
+            <center><h1>Porfolio</h1></center>
+          <br>
+                <img src="phpcrud.png" class="rounded float-left" width="350px" height="230px; border-radius: 15px;">
+                <img src="javaphp.png" class="rounded float-right" width="350px" height="230px; border-radius: 15px;">
+                  <center><div style="background-color: aquamarine; width:1000px; height:150px;" >
+                      <h4  class="rounded float-left" >Aplikasi Cread, Read, Update, Delete dengan menggunakan PHP dan Java menggunakan Visual Studio Code dan  Netsbeans</h4>
+                  </div></center> 
+          </div></center>
+              
+          
+              <center><div style="background-color: #00CED1; width:1000px; height:460px; border-radius: 15px;">
+                <br>
+                    <img src="kalkulator.png" class="rounded float-left" width="350px" height="260px; border-radius: 15px;">
+                    <img src="fisika.png" class="rounded float-right" width="350px" height="350px; border-radius: 15px;">
+                      <center><div style="background-color: #00CED1; width:1000px; height:150px;" >
+                        <h4  class="rounded float-left" >Sebuah kalkulator biasa dan fisika yang dibuka di Browser dan dibuat dengan JavaScript di Visual Studio Code </h4>
+                      </div></center> 
+              </div></center>
+
+          <center><div style="background-color: aquamarine; width:1000px; height:430px; border-radius: 15px;">
+          <br>
+                <img src="jualbeli.png" class="rounded float-left" width="350px" height="230px; border-radius: 15px;">
+                <img src="web.png" class="rounded float-right" width="350px" height="230px; border-radius: 15px;">
+                <center><div style="background-color: aquamarine; width:1000px; height:150px;" >
+                      <h4  class="rounded float-left" >Sebuah Website jual beli yang terbagi menjadi 2 bagian yaitu bagian tampil alert saja dan yang satunya menampilkan Web keseluruhan</h4>
+                </div></center> 
+          </div></center>
+      </div>
+
+
+
+
+
+
+  </div>
+  </body>
+</html>
